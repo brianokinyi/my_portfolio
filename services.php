bedcb3aed831f92393c515abd87193f6ddfc93d9
brianokinyi <brianokinyi.bo@gmail.com>
2017-08-04 23:38:43 +0300

    services page

---
+++ services.php
@@ -0,0 +1,159 @@
+<?php
+  require("header.php");
+?>
+
+<!--SERVICES-->
+<section class="white-bg" id="services">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <h3 class="title-small">
+          <span>Services</span>
+        </h3>
+        <p class="content-detail">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+        </p>
+      </div>
+      <div class="col-md-9 content-right">
+        <div class="row">
+          <ul class="listing-item">
+            <li>
+              <div class="col-md-4 col-sm-4">
+                <h3 class="icon-use">
+                  c
+                </h3>
+                <p class="head-sm">
+                  MARKETING
+                </p>
+                <p class="text-grey">
+                  Typi non habent claritatem insitam est usus legentis in iis qui facit eorum claritatem. 
+                </p>
+              </div>
+            </li>
+
+            <li>
+              <div class="col-md-4 col-sm-4">
+                <h3 class="icon-use">
+                  b
+                </h3>
+                <p class="head-sm">
+                  WEB DESIGN
+                </p>
+                <p class="text-grey">
+                  Typi non habent claritatem insitam est usus legentis in iis qui facit eorum claritatem. 
+                </p>
+              </div>
+            </li>
+
+            <li>
+              <div class="col-md-4 col-sm-4">
+                <h3 class="icon-use">
+                  d
+                </h3>
+                <p class="head-sm">
+                  UI
+                </p>
+                <p class="text-grey">
+                  Typi non habent claritatem insitam est usus legentis in iis qui facit eorum claritatem. 
+                </p>
+              </div>
+            </li>
+          </ul>
+
+          <ul class="listing-item">
+            <li>
+              <div class="col-md-4 col-sm-4">
+                <p class="icon-use">
+                  f
+                </p>
+                <p class="head-sm">
+                  DIRECTOR
+                </p>
+                <p class="text-grey">
+                  Typi non habent claritatem insitam est usus legentis in iis qui facit eorum claritatem. 
+                </p>
+              </div>
+            </li>
+
+            <li>
+              <div class="col-md-4 col-sm-4">
+                <h3 class="icon-use">
+                  p
+                </h3>
+                <p class="head-sm">
+                  UX DESIGN
+                </p>
+                <p class="text-grey">
+                  Typi non habent claritatem insitam est usus legentis in iis qui facit eorum claritatem. 
+                </p>
+              </div>
+            </li>
+
+            <li>
+              <div class="col-md-4 col-sm-4">
+                <h3 class="icon-use">
+                  m
+                </h3>
+                <p class="head-sm">
+                  FRONT END
+                </p>
+                <p class="text-grey">
+                  Typi non habent claritatem insitam est usus legentis in iis qui facit eorum claritatem. 
+                </p>
+              </div>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--/.SERVICES END-->
+
+<!--TESTIMONIAL-->
+<section id="testimonial">
+  <div class="container">
+    <div class="row wrap-testimonial">
+      <div class="col-md-10 col-md-offset-1">
+        <div class="owl-carousel">
+          <div class="list-testimonial">
+            <div class="content-testimonial">
+              <h3 class="testi">
+                “ Reza did a phenominal job setting up my a website for my product He performed the task very quickly and efficiently. We know we have the superior topical pain relief cream on the market and he has helped us communicate that message via the website he built from scratch. Thanks! ”
+              </h3>
+              <p class="people">
+                Darin Cohen - Active10
+              </p>
+            </div>
+          </div>
+          <div class="list-testimonial">
+            <div class="content-testimonial">
+              <h3 class="testi">
+                “ Another successful project working with Reza. He has great design skills, is very responsive and professional. Looking forward to the next opportunity to work together. ”
+              </h3>
+              <p class="people">
+                Marko Milicevic
+              </p>
+            </div>
+          </div>
+          <div class="list-testimonial">
+            <div class="content-testimonial">
+              <h3 class="testi">
+                “ Great designer! I like his sense of design, especially the color scheme. Quick turnaround. Look forward to work with him again. Thanks!! ”
+              </h3>
+              <p class="people">
+                JK Thang
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="mask-testimonial"></div>
+</section>
+<!--/.TESTMONIAL END-->
+<?php
+  require("footer.php");
+?>
