79a35ac2b6ae9913bac1358fc7d8b8801091f94d
brianokinyi <brianokinyi.bo@gmail.com>
2017-08-11 12:52:23 +0300

    three slide bars

--- employer.php
+++ employer.php
@@ -24,6 +24,7 @@
   <!-- end -->
 	
 	<style type="text/css" media="screen">
+	  body { background: #EEF0F7; }
 	 .layout { padding: 50px; font-family: Georgia, serif; }
 	 .layout-slider { margin-bottom: 60px; width: 50%; }
 	 .layout-slider-settings { font-size: 12px; padding-bottom: 10px; }
@@ -32,16 +33,36 @@
 
 </head>
 <body>
-  	<div>
+	<div class="container">
 	  <div class="layout">
-	    <div class="layout-slider">
-	      <input id="SliderSingle" type="slider" name="price" value="500000" />
+	    <div class="layout-slider" style="width: 100%">
+	      Basic Salary<span style="display: inline-block; width: 400px; padding: 0 5px;"><input id="SliderSingle" type="slider" name="price" value="500000" /></span>
 	    </div>
 	    <script type="text/javascript" charset="utf-8">
-	      jQuery("#SliderSingle").slider({ from: 30000, to: 1000000, step: 5000, round: 5, format: { format: '##', locale: 'de' }, dimension: '&nbsp;€', skin: "round" });
-	    </script> 
-	   </div>
-	</div>  
+	      jQuery("#SliderSingle").slider({ from: 30000, to: 5000000, step: 5000, round: 5, dimension: '&nbsp;Ksh', skin: "round" });
+	    </script>
+	    <!-- //End Basic Salary-->
+
+
+	    <div class="layout-slider">
+	      Total Allowances<span style="display: inline-block; width: 400px; padding: 0 5px;"><input id="Slider2" type="slider" name="price" value="100000" /></span>
+	    </div>
+	    <script type="text/javascript" charset="utf-8">
+	      jQuery("#Slider2").slider({ from: 10000, to: 500000, heterogeneity: ['50/50000'], step: 1000, dimension: '&nbsp; Ksh' });
+	    </script>
+	    <!-- //End Allowances-->
+
+	    <div class="layout-slider" style="width: 100%">
+	      Deductions <span style="display: inline-block; width: 400px; padding: 0 5px;"><input id="Slider1" type="slider" name="price" value="10000" /></span>
+	    </div>
+	    <script type="text/javascript" charset="utf-8">
+	      jQuery("#Slider1").slider({ from: 5000, to: 500000, step: 500, smooth: true, round: 0, dimension: "&nbsp;Ksh ", skin: "plastic" });
+	    </script>
+	    <!-- //End Deductions--> 
+
+	    </div><!-- //End Container--> 
+	</div> 
+     
 </body>
 </html>
 
